3127ea0ecc157e646ab5ec523e0b5fe681c8ebe6
Ярослав Латин <yaroslav.khaustovich@yandex.ru>
2022-06-11 15:03:52 +0300

    ну вроде робит

--- src/save_xml.java
+++ src/save_xml.java
@@ -26,12 +26,12 @@ public class save_xml {
         NodeList fine =  doc.getElementsByTagName("fine");
         NodeList fines = doc.getElementsByTagName("fines");
        // NodeList root =  doc.getElementsByTagName("root");
-        for(int i = 0; i < fine.getLength(); i++) fine.item(i).setTextContent("");
+        for(int i = fine.getLength()-1; i >=0 ; i--)
+        {
+            fines.item(0).removeChild(fine.item(i));
+        }
 
-        Transformer transformer = TransformerFactory.newInstance().newTransformer();
-        Source source = new DOMSource(doc);
-        Result result = new StreamResult(file);
-        transformer.transform (source, result);
+
         for(int i = 0; i < roott.getFine().size(); i++) {
             Element fine_new = doc.createElement("fine");
             fines.item(0).appendChild(fine_new);
@@ -52,6 +52,59 @@ public class save_xml {
             fine.item(i).appendChild(type);
             fine.item(i).appendChild(date);
         }
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        Source source = new DOMSource(doc);
+        Result result = new StreamResult(file);
+        transformer.transform (source, result);
+
+
+        File file1 = new File("peopleData.xml");
+        DocumentBuilderFactory dbf1 = DocumentBuilderFactory.newInstance();
+        org.w3c.dom.Document doc1 =null;
+        try{
+            doc1 = dbf1.newDocumentBuilder().parse(file1);
+        }
+        catch (Exception e)
+        {
+            return;
+        }
+        NodeList people =  doc1.getElementsByTagName("people");
+        NodeList peoples = doc1.getElementsByTagName("peoples");
+        // NodeList root =  doc.getElementsByTagName("root");
+        for(int i = people.getLength()-1; i >=0 ; i--)
+        {
+            peoples.item(0).removeChild(people.item(i));
+        }
+
+
+        for(int i = 0; i < roott.getPeople().size(); i++) {
+            Element people_new = doc1.createElement("people");
+            peoples.item(0).appendChild(people_new);
+
+            Element key = doc1.createElement("key");
+            Element name = doc1.createElement("name");
+            Element pass = doc1.createElement("pass");
+            Element goss = doc1.createElement("goss_number");
+            Element mark = doc1.createElement("mark");
+            Element date = doc1.createElement("date_techn_inspect");
+            key.setTextContent( String.valueOf(roott.getPeople().get(i).getKey()));
+            name.setTextContent( String.valueOf(roott.getPeople().get(i).getName()));
+            pass.setTextContent( String.valueOf(roott.getPeople().get(i).getPass()));
+            goss.setTextContent( String.valueOf(roott.getPeople().get(i).getGoss_number()));
+            mark.setTextContent( String.valueOf(roott.getPeople().get(i).getMark()));
+            date.setTextContent( String.valueOf(roott.getPeople().get(i).getDate_techn_inspect()));
+
+            people.item(i).appendChild(key);
+            people.item(i).appendChild(name);
+            people.item(i).appendChild(pass);
+            people.item(i).appendChild(goss);
+            people.item(i).appendChild(mark);
+            people.item(i).appendChild(date);
+        }
+        Transformer transformer1 = TransformerFactory.newInstance().newTransformer();
+        Source source1 = new DOMSource(doc1);
+        Result result1 = new StreamResult(file1);
+        transformer1.transform (source1, result1);
 
 
     }
